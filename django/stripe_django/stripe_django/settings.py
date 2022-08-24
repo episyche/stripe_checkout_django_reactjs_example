b50eacfbe208a4dc56819884921761088c043997
VishaliArumugam01 <104819616+VishaliArumugam01@users.noreply.github.com>
2022-08-24 18:05:04 +0530

    Update settings.py

--- django/stripe_django/stripe_django/settings.py
+++ django/stripe_django/stripe_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8n=p9@i=j+@iccjb*z!46uh5j@ltlil6fnyy73(6w135g)p*=1'
+SECRET_KEY = 'django-insecure-8n=p9@i=j+@iccjb*z!46uh5j@ltlil6fnyy73(6w135g)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
